ea6811cf277d69c6b20ce7f56a75b928fa16b438
jordan smith <jordan.smith6035@gmail.com>
2022-11-29 13:18:42 -0700

    complete game class

--- src/Game.js
+++ src/Game.js
@@ -1,18 +1,33 @@
 const data = require('./data');
+const Deck = require('./Deck');
+const Round = require('./Round');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
 class Game {
-  constructor() {}
-
-  printMessage(deck, round) {
-    console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
------------------------------------------------------------------------`)
+  constructor(newRound) {
+    this.currentRound = newRound
   }
 
+
+
+  start() {
+    const newStartDeck = new Deck(prototypeQuestions)
+    this.currentRound = new Round(newStartDeck)
+    this.printMessage(newStartDeck, this.currentRound)
+    this.printMessage(this.currentRound)
+
+  }
+  printMessage(deck, round) {
+    console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
+  -----------------------------------------------------------------------`)
+  }
+  
   printQuestion(round) {
       util.main(round);
   }
-}
 
+
+
+}
 module.exports = Game;
